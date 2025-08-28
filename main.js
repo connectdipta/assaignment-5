0f58b2719a4e5e3d8106c78637f4eeda3b10514d
connectdipta <connect.dipta@gmail.com>
2025-08-28 21:02:31 +0600

    5th commit

--- main.js
+++ main.js
@@ -118,3 +118,243 @@ document.getElementById("call-btn3").addEventListener("click", function () {
     callHistoryContainer.appendChild(div);
     callHistoryContainer1.appendChild(div1);
 });
+
+
+document.getElementById("call-btn4").addEventListener("click", function () {
+  const point = document.getElementById("point-show").innerText;
+  const finalPoint = parseInt(point);
+
+  if (finalPoint < 20) {
+    alert("❌ You Dont have too much coin. You need atleast 20 point.");
+    return;
+  } else {
+    alert("Calling Ambulance 1994-999999...");
+  }
+  const data = {
+    who: "Ambulance Number",
+    date: new Date().toLocaleTimeString(),
+    number: "1994-999999",
+  };
+  
+  document.getElementById("point-show").innerText = finalPoint - 20;
+  const callHistoryContainer = document.getElementById("callHistory-parent");
+  const callHistoryContainer1 = document.getElementById("callHistory-parent1");
+  
+    const div = document.createElement("div");
+    div.innerHTML = `
+         <div
+          class="bg-zinc-50 rounded-lg mt-[8px] mb-[8px] flex items-center justify-between p-[16px]"
+        >
+          <div>
+            <h1 class="inter-font text-[18px] max-sm:text-[14px] font-semibold">
+              ${data.who}
+            </h1>
+            <p class="hind-madurai-font font-normal text-[18px]">${data.number}</p>
+          </div>
+          <h1 class="hind-madurai-font text-[18px] font-normal">${data.date}</h1>
+        </div>
+        `;
+        const div1=div.cloneNode(true);
+    callHistoryContainer.appendChild(div);
+    callHistoryContainer1.appendChild(div1);
+});
+
+document.getElementById("call-btn5").addEventListener("click", function () {
+  const point = document.getElementById("point-show").innerText;
+  const finalPoint = parseInt(point);
+
+  if (finalPoint < 20) {
+    alert("❌ You Dont have too much coin. You need atleast 20 point.");
+    return;
+  } else {
+    alert("Calling Women & Child Helpline 109...");
+  }
+  const data = {
+    who: "Women & Child Helpline Number",
+    date: new Date().toLocaleTimeString(),
+    number: 109,
+  };
+
+  callHistory.push(data);
+  document.getElementById("point-show").innerText = finalPoint - 20;
+  const callHistoryContainer = document.getElementById("callHistory-parent");
+  const callHistoryContainer1 = document.getElementById("callHistory-parent1");
+  
+    const div = document.createElement("div");
+    div.innerHTML = `
+         <div
+          class="bg-zinc-50 rounded-lg mt-[8px] mb-[8px] flex items-center justify-between p-[16px]"
+        >
+          <div>
+            <h1 class="inter-font text-[18px] max-sm:text-[14px] font-semibold">
+              ${data.who}
+            </h1>
+            <p class="hind-madurai-font font-normal text-[18px]">${data.number}</p>
+          </div>
+          <h1 class="hind-madurai-font text-[18px] font-normal">${data.date}</h1>
+        </div>
+        `;
+        const div1=div.cloneNode(true);
+    callHistoryContainer.appendChild(div);
+    callHistoryContainer1.appendChild(div1);
+});
+
+document.getElementById("call-btn6").addEventListener("click", function () {
+  const point = document.getElementById("point-show").innerText;
+  const finalPoint = parseInt(point);
+
+  if (finalPoint < 20) {
+    alert("❌ You Dont have too much coin. You need atleast 20 point.");
+    return;
+  } else {
+    alert("Calling Anti-Corruption 106...");
+  }
+  const data = {
+    who: "Anti-Corruption Number",
+    date: new Date().toLocaleTimeString(),
+    number: 106,
+  };
+
+  callHistory.push(data);
+  document.getElementById("point-show").innerText = finalPoint - 20;
+  const callHistoryContainer = document.getElementById("callHistory-parent");
+  const callHistoryContainer1 = document.getElementById("callHistory-parent1");
+  
+    const div = document.createElement("div");
+    div.innerHTML = `
+         <div
+          class="bg-zinc-50 rounded-lg mt-[8px] mb-[8px] flex items-center justify-between p-[16px]"
+        >
+          <div>
+            <h1 class="inter-font text-[18px] max-sm:text-[14px] font-semibold">
+              ${data.who}
+            </h1>
+            <p class="hind-madurai-font font-normal text-[18px]">${data.number}</p>
+          </div>
+          <h1 class="hind-madurai-font text-[18px] font-normal">${data.date}</h1>
+        </div>
+        `;
+        const div1=div.cloneNode(true);
+    callHistoryContainer.appendChild(div);
+    callHistoryContainer1.appendChild(div1);
+});
+
+document.getElementById("call-btn7").addEventListener("click", function () {
+  const point = document.getElementById("point-show").innerText;
+  const finalPoint = parseInt(point);
+
+  if (finalPoint < 20) {
+    alert("❌ You Dont have too much coin. You need atleast 20 point.");
+    return;
+  } else {
+    alert("Calling Electricity Outage 16216...");
+  }
+  const data = {
+    who: "Electricity Outage Number",
+    date: new Date().toLocaleTimeString(),
+    number: 16216,
+  };
+
+  callHistory.push(data);
+  document.getElementById("point-show").innerText = finalPoint - 20;
+  const callHistoryContainer = document.getElementById("callHistory-parent");
+  const callHistoryContainer1 = document.getElementById("callHistory-parent1");
+  
+    const div = document.createElement("div");
+    div.innerHTML = `
+         <div
+          class="bg-zinc-50 rounded-lg mt-[8px] mb-[8px] flex items-center justify-between p-[16px]"
+        >
+          <div>
+            <h1 class="inter-font text-[18px] max-sm:text-[14px] font-semibold">
+              ${data.who}
+            </h1>
+            <p class="hind-madurai-font font-normal text-[18px]">${data.number}</p>
+          </div>
+          <h1 class="hind-madurai-font text-[18px] font-normal">${data.date}</h1>
+        </div>
+        `;
+        const div1=div.cloneNode(true);
+    callHistoryContainer.appendChild(div);
+    callHistoryContainer1.appendChild(div1);
+});
+
+document.getElementById("call-btn8").addEventListener("click", function () {
+  const point = document.getElementById("point-show").innerText;
+  const finalPoint = parseInt(point);
+
+  if (finalPoint < 20) {
+    alert("❌ You Dont have too much coin. You need atleast 20 point.");
+    return;
+  } else {
+    alert("Calling Brac 16445...");
+  }
+  const data = {
+    who: "Brac Number",
+    date: new Date().toLocaleTimeString(),
+    number: 16445,
+  };
+
+  callHistory.push(data);
+  document.getElementById("point-show").innerText = finalPoint - 20;
+  const callHistoryContainer = document.getElementById("callHistory-parent");
+  const callHistoryContainer1 = document.getElementById("callHistory-parent1");
+  
+    const div = document.createElement("div");
+    div.innerHTML = `
+         <div
+          class="bg-zinc-50 rounded-lg mt-[8px] mb-[8px] flex items-center justify-between p-[16px]"
+        >
+          <div>
+            <h1 class="inter-font text-[18px] max-sm:text-[14px] font-semibold">
+              ${data.who}
+            </h1>
+            <p class="hind-madurai-font font-normal text-[18px]">${data.number}</p>
+          </div>
+          <h1 class="hind-madurai-font text-[18px] font-normal">${data.date}</h1>
+        </div>
+        `;
+        const div1=div.cloneNode(true);
+    callHistoryContainer.appendChild(div);
+    callHistoryContainer1.appendChild(div1);
+});
+
+document.getElementById("call-btn9").addEventListener("click", function () {
+  const point = document.getElementById("point-show").innerText;
+  const finalPoint = parseInt(point);
+
+  if (finalPoint < 20) {
+    alert("❌ You Dont have too much coin. You need atleast 20 point.");
+    return;
+  } else {
+    alert("Calling Bangladesh Railway 163...");
+  }
+  const data = {
+    who: "Bangladesh Railway Number",
+    date: new Date().toLocaleTimeString(),
+    number: 163,
+  };
+
+  callHistory.push(data);
+  document.getElementById("point-show").innerText = finalPoint - 20;
+  const callHistoryContainer = document.getElementById("callHistory-parent");
+  const callHistoryContainer1 = document.getElementById("callHistory-parent1");
+  
+    const div = document.createElement("div");
+    div.innerHTML = `
+         <div
+          class="bg-zinc-50 rounded-lg mt-[8px] mb-[8px] flex items-center justify-between p-[16px]"
+        >
+          <div>
+            <h1 class="inter-font text-[18px] max-sm:text-[14px] font-semibold">
+              ${data.who}
+            </h1>
+            <p class="hind-madurai-font font-normal text-[18px]">${data.number}</p>
+          </div>
+          <h1 class="hind-madurai-font text-[18px] font-normal">${data.date}</h1>
+        </div>
+        `;
+        const div1=div.cloneNode(true);
+    callHistoryContainer.appendChild(div);
+    callHistoryContainer1.appendChild(div1);
+});
---
+++ readme.md
@@ -0,0 +1,24 @@
+**1. What is the difference between getElementById, getElementsByClassName, and querySelector / querySelectorAll?**
+**Solution:** 
+  1. getElementById: It selects one element by it's unique ID. 
+  2. getElementsByClassName: It selects a live collection of all elements which have a class name. 
+  3. querySelector: It selects the first element which matches a specific CSS selector. 
+  4. querySelectorAll: It selects a static list of all elements which matches a specific CSS selector.
+
+**2. How do you create and insert a new element into the DOM?**
+**Solution:**
+  Here I create an element through using the .createElement() function and then insert it using .appendChild() .
+
+**3. What is Event Bubbling and how does it work?**
+**Solution:** 
+  Event bubbling means when we click on a child element or trigger an event, that event also moves up and triggers on its parent elements too.
+
+**4. What is Event Delegation in JavaScript? Why is it useful?**
+**Solution:** 
+  Attaching a single event listener to a parent element is known as event delegation, and it is a useful method. This listener controls the events of all its child elements. It uses event bubbling to capture events from the child elements. This method works well for improving performance and controlling events on dynamically introduced content.
+  Event delegation means adding one event listener to a parent element instead of putting listeners on every child. For event bubbling, the parent can catch events from its children. It’s useful for performance and also works well when new child elements are added later.
+
+**5. What are the differences between the preventDefault() and stopPropagation() methods?**
+**Solution:**
+  1. preventDefault() function stops the browser's default response to an event. For example, it could prevent a form from submitting or a link from functioning. 
+  2. StopPropagation() stops the event bubbling process. In this way, the event is kept from rising.
